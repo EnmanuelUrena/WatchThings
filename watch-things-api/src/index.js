f274388d90925ac7c0ea20046ac91b325560ef7e
enmanuelurenav <enmanuelurenav@gmail.com>
2023-02-09 14:17:46 -0400

    add a new login endpoint for posts to the api

--- watch-things-api/src/index.js
+++ watch-things-api/src/index.js
@@ -6,4 +6,10 @@ app.get('/', (c) => {
   return c.json('Hello World')
 })
 
+app.post('/login', (c) => {
+  const { username, password } = c.body
+  username && password ? console.log('login') : console.log('Missing fields')
+  return c.json('login')
+})
+
 export default app
